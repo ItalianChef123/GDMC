5a328e668742d0b3372fbb35135f59733cd732e3
ItalianChef123 <Ollie-Thornton@hotmail.com>
2024-05-29 19:23:09 +0100

    Added road building function

---
+++ main.py
@@ -0,0 +1,59 @@
+import gdpc
+from gdpc import geometry
+import math
+
+editor = gdpc.Editor()
+buildArea = editor.getBuildArea()
+buildRect = buildArea.toRect()
+print("Loading world slice")
+worldSlice = editor.loadWorldSlice(buildRect)
+print("World slice loaded")
+heightMap = worldSlice.heightmaps["WORLD_SURFACE"]
+building_positions = [(46, 119, -2), (37, 119, 19)]
+
+
+def build_roads(positions):
+    for i in range(len(positions)):
+        for j in range(len(positions)):
+            if i != j:
+                xDifference = positions[j][0] - positions[i][0]
+                yDifference = positions[j][1] - positions[i][1]
+                zDifference = positions[j][2] - positions[i][2]
+                horizontalGradient = int(zDifference / xDifference)
+                additionalHorizontalChange = int(((zDifference / xDifference) - horizontalGradient) * xDifference)
+                GCD = math.gcd(additionalHorizontalChange, xDifference)
+                additionalHorizontalChange = additionalHorizontalChange / GCD
+                additionalHorizontalChangeInterval = xDifference / GCD
+                GCD = math.gcd(xDifference, math.gcd(yDifference, zDifference))
+                yInterval = yDifference / GCD
+                currentX = positions[i][0]
+                currentY = positions[i][1]
+                currentZ = positions[i][2]
+                finalX = positions[j][0]
+                if xDifference < 0:
+                    xChange = -1
+                else:
+                    xChange = 1
+                l = 0
+                for k in range(xDifference):
+                    nextZ = currentZ + horizontalGradient
+                    if l != additionalHorizontalChange:
+                        if l < additionalHorizontalChange:
+                            l = l + 1
+                            nextZ = nextZ + 1
+                        else:
+                            l = l - 1
+                            nextZ = nextZ - 1
+                    elif l == additionalHorizontalChangeInterval:
+                        l = 0
+                    geometry.placeCuboid(editor, (currentX, currentY, currentZ),
+                                         (currentX + xChange, currentY + yInterval, nextZ),
+                                         gdpc.Block("dirt_path"))
+                    currentX = currentX + xChange
+                    currentY = currentY + yInterval
+                    currentZ = nextZ
+                    if currentX == finalX:
+                        break
+
+
+build_roads(building_positions)
